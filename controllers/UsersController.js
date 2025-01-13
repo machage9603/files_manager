a18c000fe56b96eb2b409853b8cdc59b5a6cbd21
machage9603 <mikemachage@gmail.com>
2025-01-13 23:32:17 +0300

    add task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,23 +1,31 @@
-import Queue from 'bull';
-import UsersCollection from '../utils/users';
+#!/usr/bin/node
 
-const userQueue = Queue('send welcome email');
+const dbClient = require('../utils/db');
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (email === undefined) {
+    if (!email) {
       res.status(400).json({ error: 'Missing email' });
-    } else if (password === undefined) {
-      res.status(400).json({ error: 'Missing password' });
-    } else if (await UsersCollection.getUser({ email })) {
-      res.status(400).json({ error: 'Already exist' });
-    } else {
-      const userId = await UsersCollection.createUser(email, password);
-      userQueue.add({ userId });
-      res.status(201).json({ id: userId, email });
+      res.end();
+      return;
     }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      res.end();
+      return;
+    }
+    const userExist = await dbClient.userExist(email);
+    if (userExist) {
+      res.status(400).json({ error: 'Already exist' });
+      res.end();
+      return;
+    }
+    const user = await dbClient.createUser(email, password);
+    const id = `${user.insertedId}`;
+    res.status(201).json({ id, email });
+    res.end();
   }
 }
 
-export default UsersController;
+module.exports = UsersController;
--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const { MongoClient } = require('mongodb');
 const mongo = require('mongodb');
 const { pwdHashed } = require('./utils');
