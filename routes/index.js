1085c22fea5f2abc7aa91145f9928d0a187b288a
machage9603 <mikemachage@gmail.com>
2025-01-15 23:33:01 +0300

    add task 3

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 const route = (app) => {
@@ -11,6 +12,10 @@ const route = (app) => {
 
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
   });
 };
 
