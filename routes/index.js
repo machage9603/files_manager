595888f0f6dd9d0cbea78c6edc8d7eea8bed6490
machage9603 <mikemachage@gmail.com>
2025-01-16 00:26:08 +0300

    add task 4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 const route = (app) => {
@@ -16,6 +17,18 @@ const route = (app) => {
 
   router.post('/users', (req, res) => {
     UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 };
 
